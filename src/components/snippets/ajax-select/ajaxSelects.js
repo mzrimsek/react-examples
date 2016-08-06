19bf964c1ea8986c69a18e42db27a182e428cd79
Mike Zrimsek <mikezrimsek@gmail.com>
2016-08-06 10:04:35 -0400

    added placeholders for first snippets

---
+++ src/components/snippets/ajax-select/ajaxSelects.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class AjaxSelects extends Component{
+  render(){
+    return(
+      <div className="ajaxSelects">
+
+      </div>
+    )
+  }
+}
