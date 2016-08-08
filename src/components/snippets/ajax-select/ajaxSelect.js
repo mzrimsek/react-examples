82a2ae07bb5868403fdeb005d0f7c8adc9451b01
Mike Zrimsek <mikezrimsek@gmail.com>
2016-08-08 04:53:39 -0400

    data is getting loaded for selects and currently selected option...next is displaying data and maybe fixing some styles

--- src/components/snippets/ajax-select/ajaxSelect.js
+++ src/components/snippets/ajax-select/ajaxSelect.js
@@ -9,10 +9,15 @@ export default class AjaxSelect extends Component{
     this.state = {
       primarySelectData: this.getPrimarySelectData(),
       secondarySelectData: [],
-      selectedItem: {}
+      selectedItemIndex: 0,
+      selectedItemData: {},
+      loadingSelectedItemData: false
     };
+
     this.getSecondarySelectData = this.getSecondarySelectData.bind(this);
+    this.getSecondarySelectDataToDisplay = this.getSecondarySelectDataToDisplay.bind(this);
     this.updateSelectedItem = this.updateSelectedItem.bind(this);
+    this.getSelectedItemData = this.getSelectedItemData.bind(this);
   }
   getPrimarySelectData(){
     let primarySelectDisplayBase = 'Generation ';
@@ -23,29 +28,62 @@ export default class AjaxSelect extends Component{
     return primarySelectData;
   }
   getSecondarySelectData(generationNumber){
-    console.log(generationNumber);
-    let dataUrl = 'http://pokeapi.co/api/v2/generation/' + generationNumber + '/';
-    axios.get(dataUrl)
-      .then(function(res){
-        this.setState({
-          secondarySelectData: res.data.pokemon_species
-        });
-      }.bind(this))
-      .catch(function(err){
-        console.error(err);
-      }.bind(this));
+    if(generationNumber !== 0){
+      let dataUrl = 'http://pokeapi.co/api/v2/generation/' + generationNumber + '/';
+      axios.get(dataUrl)
+        .then(function(res){
+          this.setState({
+            secondarySelectData: res.data.pokemon_species
+          });
+        }.bind(this))
+        .catch(function(err){
+          console.error(err);
+        }.bind(this));
+    }
   }
-  updateSelectedItem(event){
-    let value = event.target.value;
-    this.setState({
-      selectedItem: value
-    });
+  getSecondarySelectDataToDisplay(){
+    let data = this.state.secondarySelectData;
+    let namesOnly = ['Select Pokemon'];
+    for(var i = 0; i < data.length; i++){
+      namesOnly.push(data[i].name);
+    }
+    return namesOnly;
+  }
+  updateSelectedItem(selectedItemIndex){
+    this.setState({ selectedItemIndex: selectedItemIndex });
+  }
+  getSelectedItemData(){
+    if(this.state.selectedItemIndex !== 0){
+      this.setState({ loadingSelectedItemData: true });
+      let selectedItem = this.state.secondarySelectData[this.state.selectedItemIndex-1];
+      axios.get(selectedItem.url)
+        .then(function(res){
+          this.setState({
+            selectedItemData: res.data,
+            loadingSelectedItemData: false
+          });
+        }.bind(this))
+        .catch(function(err){
+          console.error(err);
+        }.bind(this));
+    }
   }
   render(){
+    let secondarySelectData = this.getSecondarySelectDataToDisplay();
+    let buttonClass = 'button is-primary' + (this.state.loadingSelectedItemData ? ' is-loading' : '');
     return(
       <div className="ajax-selects">
-        <SelectWrapper label="Pokemon Generation" data={this.state.primarySelectData} onChange={this.getSecondarySelectData} />
-        <SelectWrapper label="Pokemon" data={this.state.secondarySelectData} onChange={this.updateSelectedItem} />
+        <div className="inputs">
+          <div className="control">
+            <SelectWrapper label="Pokemon Generation" data={this.state.primarySelectData} onChange={this.getSecondarySelectData} />
+          </div>
+          <div className="control">
+            <SelectWrapper label="Pokemon" data={secondarySelectData} onChange={this.updateSelectedItem} />
+          </div>
+          <div className="control">
+            <button className={buttonClass} onClick={this.getSelectedItemData}>Go</button>
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/snippets/ajax-select/selectWrapper.js
+++ src/components/snippets/ajax-select/selectWrapper.js
@@ -1,13 +1,5 @@
 import React, {Component} from 'react';
 
-class SelectOption extends Component{
-  render(){
-    return(
-      <option className="select-option" value={this.props.value}>{this.props.display}</option>
-    );
-  }
-}
-
 export default class SelectWrapper extends Component{
   constructor(props){
     super(props);
@@ -20,9 +12,9 @@ export default class SelectWrapper extends Component{
   render(){
     let selectOptionNodes = this.props.data.map(function(data, index){
       return(
-        <SelectOption key={index} display={data} value={index} />
+        <option className="select-option" key={index} value={index}>{data}</option>
       );
-    })
+    });
     return(
       <div className="select-wrapper">
         <label className="label">{this.props.label}:</label>
